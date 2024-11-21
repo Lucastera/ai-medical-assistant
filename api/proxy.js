b5b3907bcb9e0e39f881494ac6f772f567bc5860
Lucas <shinono963@gmail.com>
2024-11-21 19:30:11 +0800

    feat: deploy

--- api/proxy.js
+++ api/proxy.js
@@ -1,10 +1,11 @@
 // 该服务为 vercel serve跨域处理
 import { createProxyMiddleware } from 'http-proxy-middleware';
-module.exports = (req, res) => {
+
+export default (req, res) => {
   let target = "";
   if (req.url.startsWith("/api")) {
-    //这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
-    target = "http://103.194.106.195:8000"; //这里就是在vite中配置的一样
+    // 这里使用/api可能会与vercel serverless 的 api 路径冲突，根据接口进行调整
+    target = "http://103.194.106.195:8000"; // 这里就是在vite中配置的一样
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
