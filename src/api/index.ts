0346ac5d488e8f533aa94d680b40f3475084b451
Lucas <shinono963@gmail.com>
2024-11-21 15:39:02 +0800

    fix: fix ui bugs

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,25 +1,25 @@
-import request from './request';
+// import request from './request';
 
-export const getCodeReviewHistory = (params) => {
-    return request({
-        url: '/review/history',
-        method: 'get',
-        params
-    });
-};
+// export const getCodeReviewHistory = (params) => {
+//     return request({
+//         url: '/review/history',
+//         method: 'get',
+//         params
+//     });
+// };
 
-export const submitCodeReview = (data) => {
-    return request({
-        url: '/review/submit',
-        method: 'post',
-        data
-    });
-};
+// export const submitCodeReview = (data) => {
+//     return request({
+//         url: '/review/submit',
+//         method: 'post',
+//         data
+//     });
+// };
 
-export const getReviewDetailHistory = (historyID) => {
-    return request({
-      url: `/review/detail?history_id=${historyID}`, // Passing reviewId as a query parameter
-      method: 'get'
-    });
-  };
+// export const getReviewDetailHistory = (historyID) => {
+//     return request({
+//       url: `/review/detail?history_id=${historyID}`, // Passing reviewId as a query parameter
+//       method: 'get'
+//     });
+//   };
 
